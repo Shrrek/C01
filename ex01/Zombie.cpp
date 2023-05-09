59748530c267b0cbbdf86e24797d85dd19765fb7
Shrrek <85354673+Shrrek@users.noreply.github.com>
2023-05-09 12:57:34 +0200

    Add files via upload

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie::Zombie(){
+
+}
+
+/*Zombie::Zombie(std::string name){
+	this->_name = name;
+}*/
+
+Zombie::~Zombie(){
+	std::cout<<this->_name<<": closed."<<std::endl;
+}
+
+void Zombie::announce (void){
+	std::cout<<this->_name<<": BraiiiiiiinnnzzzZ..."<<std::endl;
+}
+
+void Zombie::setName(std::string name){
+	this->_name = name;
+}
